4d829b5f8966b294c5b58e1fc559ed43f074ce91
Abhishek Patil <abishkpatil@gmail.com>
2024-08-30 01:43:44 +0530

    minor fixes and changes

--- gmailgenius-firebase/src/pages/Agent.jsx
+++ gmailgenius-firebase/src/pages/Agent.jsx
@@ -10,7 +10,7 @@ const Agent = ({ user }) => {
         <section className="bg-white dark:bg-gray-900 mt-12">
             <div className="py-8 px-4 mx-auto max-w-screen-xl text-center lg:py-16 lg:px-12">
                 <span className="font-semibold text-3xl text-gray-900">Enter Keywords (Crisp & Concise)</span>
-                <AddAgent />
+                <AddAgent user={user}/>
             </div>
         </section>
     );
