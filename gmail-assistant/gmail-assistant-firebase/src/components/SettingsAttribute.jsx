2a5001f2d4a4b819cf430e9cd178654585a338d7
Abhishek Patil <abishkpatil@gmail.com>
2024-09-01 02:17:20 +0530

    feat: add textArea input

--- gmail-assistant/gmail-assistant-firebase/src/components/SettingsAttribute.jsx
+++ gmail-assistant/gmail-assistant-firebase/src/components/SettingsAttribute.jsx
@@ -1,14 +1,21 @@
 import SmallButton from "./SmallButton";
 import { Audio } from 'react-loader-spinner'
-const SettingsAttribute = ({ type, displayName, value, linkAction, loading, buttonName="Link", showButton=true }) => {
+const SettingsAttribute = ({ type, displayName, value, linkAction, loading, buttonName="Link", showButton=true, textArea=false, onChangeFunction }) => {
     return <div className="flex items-center gap-4">
         <label htmlFor={type} className="text-left block text-lg font-medium text-gray-900 dark:text-white">{displayName}: </label>
-        <input
+        {textArea ? <textarea
+            id={type}
+            className="ml-auto focus:outline-none block py-1.5 px-3 w-1/2 text-md text-gray-500 bg-gray-50 rounded-lg border border-gray-300 "
+            placeholder={value}
+            onChange={(e) => {
+                onChangeFunction(e.target.value);
+            }}
+        ></textarea> : <input
             id={type}
             className="ml-auto focus:outline-none cursor-default block py-1.5 px-3 w-1/2 text-md text-gray-500 bg-gray-50 rounded-lg border border-gray-300 "
             value={value}
             readOnly
-        ></input>
+        ></input>}
         {showButton && <SmallButton
             name={loading ? <Audio
                 height="15"
