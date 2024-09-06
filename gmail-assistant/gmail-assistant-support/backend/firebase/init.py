4f9ac854fefbf74db1d11cc22cad95c4af27b395
Abhishek Patil <abishkpatil@gmail.com>
2024-09-06 19:53:30 +0530

    fix: using firebase config file

--- gmail-assistant/gmail-assistant-support/backend/firebase/init.py
+++ gmail-assistant/gmail-assistant-support/backend/firebase/init.py
@@ -3,20 +3,20 @@ from firebase_admin import credentials, auth, firestore
 from pathlib import Path
 import os
 
-# cred = credentials.Certificate(f"{Path.cwd()}/firebase/genius-57d8d-firebase-adminsdk-ue7u9-90656332c6.json")
-creds = {
-    "type": os.environ.get("type"),
-    "project_id": os.environ.get("project_id"),
-    "private_key_id": os.environ.get("private_key_id"),
-    "private_key": os.environ.get("private_key"),
-    "client_email": os.environ.get("client_email"),
-    "client_id": os.environ.get("client_id"),
-    "auth_uri": os.environ.get("auth_uri"),
-    "token_uri": os.environ.get("token_uri"),
-    "auth_provider_x509_cert_url":
-    os.environ.get("auth_provider_x509_cert_url"),
-    "client_x509_cert_url": os.environ.get("client_x509_cert_url"),
-}
+cred = credentials.Certificate(f"{Path.cwd()}/firebase/support-bot-49f93-94ae307979d3.json")
+# creds = {
+#     "type": os.environ.get("type"),
+#     "project_id": os.environ.get("project_id"),
+#     "private_key_id": os.environ.get("private_key_id"),
+#     "private_key": os.environ.get("private_key"),
+#     "client_email": os.environ.get("client_email"),
+#     "client_id": os.environ.get("client_id"),
+#     "auth_uri": os.environ.get("auth_uri"),
+#     "token_uri": os.environ.get("token_uri"),
+#     "auth_provider_x509_cert_url":
+#     os.environ.get("auth_provider_x509_cert_url"),
+#     "client_x509_cert_url": os.environ.get("client_x509_cert_url"),
+# }
 firebase_admin.initialize_app(cred)
 
 db = firestore.client()
