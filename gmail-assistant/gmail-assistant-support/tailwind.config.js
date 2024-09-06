56b5f9c3999e78137f2ccf11c94fd18d14980783
Abhishek Patil <abishkpatil@gmail.com>
2024-09-06 21:42:22 +0530

    feat: add highlighting animaton effect

--- gmail-assistant/gmail-assistant-support/tailwind.config.js
+++ gmail-assistant/gmail-assistant-support/tailwind.config.js
@@ -5,6 +5,15 @@ export default {
     extend: {
       colors: {
         primary: {"50":"#faf5ff","100":"#f3e8ff","200":"#e9d5ff","300":"#d8b4fe","400":"#c084fc","500":"#a855f7","600":"#9333ea","700":"#7e22ce","800":"#6b21a8","900":"#581c87","950":"#3b0764"}
+      },
+      keyframes: {
+        glow: {
+          '0%, 100%': { boxShadow: '0 0 1px #fde68a, 0 0 4px #fde68a' },
+          '50%': { boxShadow: '0 0 8px #fde68a, 0 0 8px #fde68a' },
+        }
+      },
+      animation: {
+        glow: 'glow 1s ease-in-out infinite',
       }
     },
     fontFamily: {
@@ -50,9 +59,6 @@ export default {
     "./node_modules/flowbite-react/lib/esm/**/*.js",
     "./node_modules/flowbite/**/*.js"
   ],
-  theme: {
-    extend: {},
-  },
   plugins: [
     require('flowbite/plugin')
   ],
