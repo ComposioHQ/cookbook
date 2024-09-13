4cf82643b0f83001ad62a1dd4f17ec31506c20d5
Abhishek Patil <abishkpatil@gmail.com>
2024-09-13 14:30:41 +0530

    feat: minor changes

--- twitter-assistant/twitter-retweet-helper/src/components/SmallButton.jsx
+++ twitter-assistant/twitter-retweet-helper/src/components/SmallButton.jsx
@@ -1,11 +1,11 @@
-const SmallButton = ({ name, action, color, width = "5rem" }) => {
+const SmallButton = ({ name, action, color, width = "5rem", type="button" }) => {
     const handleClick = () => {
         console.log("button clicked");
     }
     return <button
         style={{ width: width }}
         onClick={action ? action : handleClick}
-        type="button"
+        type={type}
         className='focus:outline-none text-white bg-purple-700 hover:bg-purple-800 font-medium rounded-lg text-sm px-5 py-2.5 h-[2.5rem]'>
         {name}
     </button>
