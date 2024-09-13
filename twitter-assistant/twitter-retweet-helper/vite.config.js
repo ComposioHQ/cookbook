b82e485d65bed0098e125e35c3fd4d5c5737f84e
Abhishek Patil <abishkpatil@gmail.com>
2024-09-13 14:28:49 +0530

    add temporary config to bypass CORS error

--- twitter-assistant/twitter-retweet-helper/vite.config.js
+++ twitter-assistant/twitter-retweet-helper/vite.config.js
@@ -4,4 +4,14 @@ import react from '@vitejs/plugin-react'
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
+  // proxy to twitter api
+  server: {
+    proxy: {
+      '/twitter': {
+        target: 'https://api.twitter.com',
+        changeOrigin: true,
+        rewrite: (path) => path.replace(/^\/twitter/, ''),
+      },
+    },
+  },
 })
