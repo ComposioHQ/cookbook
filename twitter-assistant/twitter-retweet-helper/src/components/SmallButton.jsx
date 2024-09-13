78edc9cd97d4a185a4fc458255299b3931bcf8ee
Abhishek Patil <abishkpatil@gmail.com>
2024-09-13 06:37:17 +0530

    feat: modify height

--- twitter-assistant/twitter-retweet-helper/src/components/SmallButton.jsx
+++ twitter-assistant/twitter-retweet-helper/src/components/SmallButton.jsx
@@ -6,7 +6,7 @@ const SmallButton = ({ name, action, color, width = "5rem" }) => {
         style={{ width: width }}
         onClick={action ? action : handleClick}
         type="button"
-        className='focus:outline-none text-white bg-purple-700 hover:bg-purple-800 font-medium rounded-lg text-sm px-5 py-2.5 h-[2.4rem]'>
+        className='focus:outline-none text-white bg-purple-700 hover:bg-purple-800 font-medium rounded-lg text-sm px-5 py-2.5 h-[2.5rem]'>
         {name}
     </button>
 }
