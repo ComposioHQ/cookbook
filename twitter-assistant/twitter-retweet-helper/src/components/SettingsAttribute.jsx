c08a5aaee35de490aacff6b094227b0f09377f90
Abhishek Patil <abishkpatil@gmail.com>
2024-09-13 06:36:09 +0530

    feat: update input element

--- twitter-assistant/twitter-retweet-helper/src/components/SettingsAttribute.jsx
+++ twitter-assistant/twitter-retweet-helper/src/components/SettingsAttribute.jsx
@@ -11,8 +11,9 @@ const SettingsAttribute = ({ type, displayName, value, linkAction, loading, butt
                 onChangeFunction(e.target.value);
             }}
         ></textarea> : <input
+            readOnly
             id={type}
-            className="ml-auto focus:outline-none block py-1.5 px-3 w-1/2 text-md text-gray-500 bg-gray-50 rounded-lg border border-gray-300 "
+            className="cursor-default h-[2.5rem] block w-1/2 ml-auto rounded-md border-0 text-gray-900 shadow-sm ring-1 ring-inset ring-gray-300 placeholder:text-gray-400 py-1.5 px-3 focus:outline-none"
             placeholder={value}
             onChange={(e) => {
                 onChangeFunction(e.target.value);
