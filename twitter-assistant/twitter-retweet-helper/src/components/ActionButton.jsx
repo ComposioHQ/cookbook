1a72d0bae75cecb996f1ad044064ea66538b1b53
Abhishek Patil <abishkpatil@gmail.com>
2024-09-16 16:04:47 +0530

    fix: import path

--- twitter-assistant/twitter-retweet-helper/src/components/ActionButton.jsx
+++ twitter-assistant/twitter-retweet-helper/src/components/ActionButton.jsx
@@ -1,5 +1,5 @@
 import { useNavigate } from "react-router-dom";
-import { signUpWithGoogle } from "../utils/firebase/authUtils";
+import { signUpWithGoogle } from "../utils/firebase_utils";
 import { auth } from "../config/firebase";
 import { useEffect, useState } from "react";
 
