fdc39bf8718cad8c1ce54c1bb62cc360026a6af2
Abhishek Patil <abishkpatil@gmail.com>
2024-09-17 01:33:52 +0530

    feat: restructuring & refactoring

--- twitter-assistant/twitter-retweet-helper/src/pages/Settings.jsx
+++ twitter-assistant/twitter-retweet-helper/src/pages/Settings.jsx
@@ -1,7 +1,5 @@
 import { useEffect, useState, useCallback } from "react";
 import SettingsAttribute from "../components/SettingsAttribute";
-import { auth, getUserDetailsByUid, addUserToAuthorisedUsers } from "../config/firebase";
-import axios from "axios";
 import { checkConnectionStatus, linkTwitterAccount } from "../utils/composio_utils";
 import { useSnackbar } from 'notistack'
 import Separator from "../components/Separator";
@@ -12,29 +10,10 @@ const Settings = ({ user }) => {
     const [username, setUsername] = useState("");
     const [twitterAccount, setTwitterAccount] = useState("No connected account");
     const [twitterAccountLoading, setTwitterAccountLoading] = useState(false);
-    const [userDetails, setUserDetails] = useState(null);
-    const [authorisedUsers, setAuthorisedUsers] = useState([]);
-
-    const fetchUserDetails = async () => {
-        try {
-            const details = await getUserDetailsByUid(user.uid);
-            setUserDetails(details);
-            return details;
-        } catch (error) {
-            console.error("Error fetching user details:", error);
-        }
-    };
 
     useEffect(() => {
-        const initializeAuthorisedUsersData = async () => {
-            const details = await fetchUserDetails();
-            if (details && details.authorisedUsers) {
-                setAuthorisedUsers(details.authorisedUsers);
-            }
-        };
         checkConnectionStatus("TWITTER", setTwitterAccount, user.email.split("@")[0]);
         setUsername(user.email.split("@")[0]);
-        initializeAuthorisedUsersData();
     }, [user.uid, checkConnectionStatus]);
 
     const handleLinkTwitterAccount = () => {
@@ -46,7 +25,7 @@ const Settings = ({ user }) => {
         <SettingsAttribute type="username" displayName="Composio Account" value={username} linkAction={() => { enqueueSnackbar("Account already connected", { variant: 'success' }) }} loading={false} />
         <SettingsAttribute type="twitter" displayName="Twitter Account" value={twitterAccount} linkAction={handleLinkTwitterAccount} loading={twitterAccountLoading} />
         <br />
-        <AddNewUser authorisedUsers={authorisedUsers} setAuthorisedUsers={setAuthorisedUsers} user={user} />
+        <AddNewUser user={user} />
     </div>
 };
 
