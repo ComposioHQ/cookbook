7293f641a6277be54c77f147cd886d350a51fe90
Abhishek Patil <abishkpatil@gmail.com>
2024-09-16 17:25:36 +0530

    feat: add skeleton loader | remove commented code

--- twitter-assistant/twitter-retweet-helper/src/components/AddedUsers.jsx
+++ twitter-assistant/twitter-retweet-helper/src/components/AddedUsers.jsx
@@ -2,33 +2,30 @@ import TwitterUserCard from "./TwitterUserCard";
 import { checkConnectionStatus } from "../utils/composio_utils";
 import { useState, useEffect, useCallback } from "react";
 import { linkTwitterAccount } from "../utils/composio_utils";
+import SkeletonLoader from "./SkeletonLoader";
 
 const AddedUsers = ({ authorisedUsers }) => {
     const [connectedUsers, setConnectedUsers] = useState([]);
     const [connected, setConnected] = useState(false);
+    const [fetchingUsers, setFetchingUsers] = useState(false);
     const linkAccount = async (user) => {
         await linkTwitterAccount(user);
     }
 
     const checkUsersConnection = useCallback(async () => {
+        setFetchingUsers(true);
         const updatedUsers = await Promise.all(
-            // authorisedUsers.map(async (user) => {
-            //     let isConnected = false;
-            //     await checkConnectionStatus("TWITTER", (status) => {
-            //         isConnected = status === "Connected";
-            //     }, user.username);
-            //     return { ...user, isConnected };
-            // })
             authorisedUsers.map(async (user) => {
                 let isConnected = false;
                 let res = await checkConnectionStatus("TWITTER", setConnected, user.username);
-                if(res==="yes"){
+                if (res === "yes") {
                     isConnected = true;
                 }
                 return { ...user, isConnected };
             })
         );
         setConnectedUsers(updatedUsers);
+        setFetchingUsers(false);
     }, [authorisedUsers]);
 
     useEffect(() => {
@@ -37,12 +34,17 @@ const AddedUsers = ({ authorisedUsers }) => {
 
     return (
         <div className="flex flex-col justify-center gap-4">
-            {connectedUsers.length > 0 ? (
-                connectedUsers.map((user) => (
-                    <TwitterUserCard user={user} key={user.id} connected={user.isConnected} action={() => linkAccount(user.username)} />
-                ))
+            {fetchingUsers ? (
+                // <p className="text-gray-500">Fetching users...</p>
+                <SkeletonLoader />
             ) : (
-                <p className="text-gray-500">No authorised users added yet.</p>
+                connectedUsers.length > 0 ? (
+                    connectedUsers.map((user) => (
+                        <TwitterUserCard user={user} key={user.id} connected={user.isConnected} action={() => linkAccount(user.username)} />
+                    ))
+                ) : (
+                    <p className="text-gray-500">No authorised users added yet.</p>
+                )
             )}
         </div>
     );
