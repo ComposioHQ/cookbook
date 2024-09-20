7686a19cf517ba1cf4983be7fd07489f9b6cb440
Abhishek Patil <abishkpatil@gmail.com>
2024-09-20 18:48:01 +0530

    minor update

--- twitter-assistant/twitter-retweet-helper/src/components/AuthorizedUsers.jsx
+++ twitter-assistant/twitter-retweet-helper/src/components/AuthorizedUsers.jsx
@@ -1,11 +1,11 @@
 import ReTweetCard from "./ReTweetCard";
 import SkeletonLoader from "./SkeletonLoader";
 
-const AuthorisedUsers = ({ authorisedUsers }) => {
+const AuthorisedUsers = ({ authorisedUsers, setAuthorisedUsers }) => {
     return (<div className="flex flex-col justify-center gap-4">
             {authorisedUsers.length > 0 ? (
                 authorisedUsers.map((user) => (
-                    <ReTweetCard user={user} key={user.id}/>
+                    <ReTweetCard user={user} key={user.id} setAuthorisedUsers={setAuthorisedUsers}/>
                 ))
             ) : (
                 <p className="text-gray-500 text-center">No authorized users</p>
